c350305c7b583efd53cc13df3b221aada07032ca
pgalindo-isart <49198719+pgalindo-isart@users.noreply.github.com>
2019-09-09 16:42:17 +0200

    Fix error when WSL is installed

--- launch_cygwin.bat
+++ launch_cygwin.bat
@@ -1,6 +1,6 @@
 call config.bat
 
 set HOME=
-set PATH=%PATH%;%CYGWIN_PATH%/bin/;%CYGWIN_PATH%/usr/x86_64-w64-mingw32/sys-root/mingw/bin
+set PATH=%CYGWIN_PATH%/bin/;%CYGWIN_PATH%/usr/x86_64-w64-mingw32/sys-root/mingw/bin;%PATH%
 
 bash --login -i -c mintty
