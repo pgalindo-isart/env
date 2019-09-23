e9bb1907687f96286903e990306e9f87d9116121
pgalindo-isart <49198719+pgalindo-isart@users.noreply.github.com>
2019-09-23 22:31:58 +0200

    Change PATH order

--- launch_vscode.bat
+++ launch_vscode.bat
@@ -1,7 +1,7 @@
 call config.bat
 
 set HOME=
-set PATH=%PATH%;%CYGWIN_PATH%/bin/;%CYGWIN_PATH%/usr/x86_64-w64-mingw32/sys-root/mingw/bin
+set PATH=%CYGWIN_PATH%/bin/;%CYGWIN_PATH%/usr/x86_64-w64-mingw32/sys-root/mingw/bin;%PATH%
 set SHELL=/bin/bash
 
 pushd vscode
