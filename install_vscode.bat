7621dab11e6f4bb1b0c65509a717aa29ec1bd17c
pgalindo-isart <49198719+pgalindo-isart@users.noreply.github.com>
2019-10-18 16:11:19 +0200

    Update vscode version

--- install_vscode.bat
+++ install_vscode.bat
@@ -1,9 +1,9 @@
 
-if not exist "VSCode-win32-x64.zip" (
-    bitsadmin /transfer "Download VSCode" /priority HIGH https://az764295.vo.msecnd.net/stable/f06011ac164ae4dc8e753a3fe7f9549844d15e35/VSCode-win32-x64-1.37.1.zip %CD%\VSCode-win32-x64.zip
+if not exist "VSCode-win32-x64-1.39.2.zip" (
+    bitsadmin /transfer "Download VSCode" /priority HIGH https://az764295.vo.msecnd.net/stable/6ab598523be7a800d7f3eb4d92d7ab9a66069390/VSCode-win32-x64-1.39.2.zip
 )
 
-powershell Expand-Archive VSCode-win32-x64.zip -DestinationPath vscode
+powershell Expand-Archive VSCode-win32-x64-1.39.2.zip -DestinationPath vscode
 
 pushd vscode\bin
 call code --user-data-dir %~dp0/vscode-userdata --extensions-dir %~dp0/vscode-extensions --install-extension ms-vscode.cpptools
