2dde2838b2df729b47ecf590fa9528a34da1bea4
pgalindo-isart <49198719+pgalindo-isart@users.noreply.github.com>
2020-05-20 15:04:50 +0200

    Update install_vscode.bat

--- install_vscode.bat
+++ install_vscode.bat
@@ -1,7 +1,7 @@
 URL=https://az764295.vo.msecnd.net/stable/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/VSCode-win32-x64-1.45.1.zip
 ZIP_FILE=VSCode-win32-x64-1.45.1.zip
 
-if not exist "VSCode-win32-x64-1.45.1.zip" (
+if not exist %ZIP_FILE% (
     bitsadmin /transfer "Download VSCode" /priority HIGH %URL% %CD%\%ZIP_FILE%
 )
 
